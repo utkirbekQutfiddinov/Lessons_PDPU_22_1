8b61a6f4ba5cc1e857a2fecfd074c0ad500da680
utkirbek <utkirbek@mail.ru>
2024-02-14 14:18:57 +0500

    .

--- src/main/java/utils/Test.java
+++ src/main/java/utils/Test.java
@@ -1,21 +1,7 @@
 package main.java.utils;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Test {
     public static void main(String[] args) {
-        String input = "salom salom 12 salom pdp pdp";
-        String pattern = "(\\b\\w+\\b) \\1";
-
-        Pattern regex = Pattern.compile(pattern);
-        Matcher matcher = regex.matcher(input);
-
-        while (matcher.find()) {
-            String repeatedWord = matcher.group();
-
-            System.out.println(repeatedWord);
-        }
 
     }
 }
